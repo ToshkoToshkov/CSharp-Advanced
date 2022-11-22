29c35b263eb858e21439d01852d1b4277f05166d
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 09:10:09 +0200

    Add files via upload

---
+++ 01.StacksAndQueues-Lab/02.StackSum/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> numbers = new Stack<int>(nums);
+
+            string command = Console.ReadLine().ToLower();
+
+            while (command != "end")
+            {
+                string[] cmd = command.Split();
+                string currcommand = cmd[0];
+
+                if (currcommand == "add")
+                {
+                    numbers.Push(int.Parse(cmd[1]));
+                    numbers.Push(int.Parse(cmd[2]));
+                }
+                else if (currcommand == "remove")
+                {
+                    int n = int.Parse(cmd[1]);
+
+                    if (numbers.Count < n)
+                    {
+                        continue;
+                    }
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        numbers.Pop();
+                    }
+                }
+
+                command = Console.ReadLine().ToLower();
+            }
+
+            var sum = numbers.Sum();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
