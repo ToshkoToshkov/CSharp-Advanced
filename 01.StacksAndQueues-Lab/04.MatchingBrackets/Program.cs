4bcb4390a084a24436b1c1dad69f2a41301a5f22
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 09:11:25 +0200

    Add files via upload

---
+++ 01.StacksAndQueues-Lab/04.MatchingBrackets/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char ch = input[i];
+
+                if (ch == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (ch == ')')
+                {
+                    int startIndex = stack.Pop();
+                    string result = input.Substring(startIndex, i - startIndex + 1);
+
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
