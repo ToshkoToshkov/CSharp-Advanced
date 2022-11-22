d037322ff89d8f8ffc70d137f92819cee1901d55
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 10:21:50 +0200

    Add files via upload

---
+++ 05.SetsAndDictionaries-Lab/05.RecordUniqueNames/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RecordUniqueNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                names.Add(input);
+            }
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
