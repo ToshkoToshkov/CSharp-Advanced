5ba24205d54d00aac2708f1f41d8cad47b8ecd91
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 11:30:32 +0200

    Add files via upload

---
+++ 11.ExamExercises-Practice/03.VetClinic/Clinic.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+namespace _03.VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> data;
+
+        public Clinic(int capacity)
+        {
+            Capacity = capacity;
+            data = new List<Pet>();
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count { get { return data.Count; } }
+
+        public void Add(Pet pet)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Pet pet = data.FirstOrDefault(p => p.Name == name);
+
+            if (pet == null)
+            {
+                return false;
+            }
+
+            data.Remove(pet);
+            return true;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            Pet pet = data.FirstOrDefault(p => p.Name == name && p.Owner == owner);
+
+            return pet;
+        }
+
+        public Pet GetOldestPet()
+        {
+            return data.OrderByDescending(p => p.Age).FirstOrDefault();
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine($"The clinic has the following patients:");
+
+            foreach (Pet item in data)
+            {
+                result.AppendLine($"Pet {item.Name} with owner: {item.Owner}");
+            }
+
+            return result.ToString();
+        }
+    }
+}
