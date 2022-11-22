b5b8d9099b2983c980ba5ce2369dcbb9296d2fb3
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 09:15:39 +0200

    Add files via upload

---
+++ 01.StacksAndQueues-Lab/06.HotPotato/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+
+            Queue<string> players = new Queue<string>(names);
+
+            int n = int.Parse(Console.ReadLine());
+
+            while (players.Count > 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    players.Enqueue(players.Dequeue());
+                }
+                Console.WriteLine($"Removed {players.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {players.Dequeue()}");
+        }
+    }
+}
