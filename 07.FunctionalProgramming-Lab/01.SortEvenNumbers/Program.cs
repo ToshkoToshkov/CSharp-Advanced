4ae5fcec6694bdbf91e7d37cc94e0715d03d5569
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 10:40:31 +0200

    Add files via upload

---
+++ 07.FunctionalProgramming-Lab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(n => n % 2 == 0)
+                .OrderBy(n => n)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", input));
+        }
+    }
+}
