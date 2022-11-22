cd6eba0bb89014d8f8a0d7f35258273a7cd35279
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 09:16:45 +0200

    Add files via upload

---
+++ 01.StacksAndQueues-Lab/08.Supermarket/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> names = new Queue<string>();
+
+            string command = Console.ReadLine();
+
+            int count = 0;
+
+            while (command != "End")
+            {
+
+                if (command == "Paid")
+                {
+                    while (names.Any())
+                    {
+                        Console.WriteLine(names.Dequeue());
+                        count--;
+                    }
+                }
+                else
+                {
+                    names.Enqueue(command);
+                    count++;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{count} people remaining.");
+        }
+    }
+}
