1f640777a4c6f0647467f8ee908841cda15d19a7
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 09:37:56 +0200

    Add files via upload

---
+++ 03.MultidimensionalArray-Lab/03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] currInput = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = currInput[col];
+                }
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < matrix.GetLength(1); i++) // column
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++) // row
+                {
+                    if (i == j)
+                    {
+                        sum += matrix[j, i];
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
