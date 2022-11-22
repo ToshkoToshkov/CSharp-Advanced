b5573e6ef17092948c4c8cb8a876514e58600315
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 11:03:29 +0200

    Add files via upload

---
+++ 08.FunctionalProgramming-Exercises/01.ActionPoint/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Action<List<string>> printNames = n => Console.WriteLine(string.Join(Environment.NewLine, n));
+
+            printNames(names);
+        }
+    }
+}
