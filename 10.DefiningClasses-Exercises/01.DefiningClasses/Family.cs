27fbe5265482998fb8b232be59854c671ff0cf7b
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 11:23:26 +0200

    Add files via upload

---
+++ 10.DefiningClasses-Exercises/01.DefiningClasses/Family.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            this.members = new List<Person>();
+        }
+
+        public List<Person> members { get; set; }
+
+        public void AddMember(Person person)
+        {
+            this.members.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person person = this.members
+                .OrderByDescending(p => p.Age)
+                .First();
+
+            return person;
+        }
+    }
+}
