f2ad801167677674797fc3bfbcb0bb89f29d86b0
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 11:04:34 +0200

    Add files via upload

---
+++ 08.FunctionalProgramming-Exercises/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split(" ").ToList();
+
+            List<string> newNames = names.Select(n => $"Sir {n}").ToList();
+
+            Action<List<string>> printNames = n => Console.WriteLine(string.Join(Environment.NewLine, n));
+
+            printNames(newNames);
+        }
+    }
+}
