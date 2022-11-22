db5a6bd1f38867d98256b3a4f5384e583f4cbffd
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 11:39:29 +0200

    Add files via upload

---
+++ 11.ExamExercises-Practice/09.Classroom/Classroom.cs
@@ -0,0 +1,85 @@
+﻿namespace _09.Classroom
+{
+    public class Classroom
+    {
+        public Classroom(int capacity)
+        {
+            Capacity = capacity;
+            students = new List<Student>();
+        }
+
+        private List<Student> students;
+
+        public int Capacity { get; set; }
+
+        public int Count
+        {
+            get { return students.Count; }
+        }
+
+
+        public string RegisterStudent(Student student)
+        {
+            if (students.Count < Capacity)
+            {
+                students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+
+            return "No seats in the classroom";
+        }
+
+        public string DismissStudent(string firsName, string lastName)
+        {
+            Student student = students.FirstOrDefault(s => s.FirstName == firsName && s.LastName == lastName);
+
+            if (student == null)
+            {
+                return "Student not found";
+            }
+
+            students.Remove(student);
+            return $"Dismissed student {student.FirstName} {student.LastName}";
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            Student student = students.FirstOrDefault(s => s.Subject == subject);
+
+            if (student == null)
+            {
+                return "No students enrolled for the subject";
+            }
+
+            List<Student> subjectList = new List<Student>();
+
+
+            return $"Subject: {subject}";
+
+            Console.WriteLine("Students:");
+
+            while (student != null)
+            {
+                subjectList.Add(student);
+            }
+
+            foreach (var item in subjectList)
+            {
+                Console.WriteLine($"{item.FirstName} {item.LastName}");
+            }
+        }
+
+        public int GetStudentsCount()
+        {
+            return students.Count;
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student student = students.FirstOrDefault
+                (s => s.FirstName == firstName && s.LastName == lastName);
+
+            return student;
+        }
+    }
+}
---
+++ 11.ExamExercises-Practice/09.Classroom/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _09.Classroom
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            // Initialize the repository
+            Classroom classroom = new Classroom(10);
+
+            // Initialize entities
+            Student student = new Student("Peter", "Parker", "Geometry");
+            Student studentTwo = new Student("Sarah", "Smith", "Algebra");
+            Student studentThree = new Student("Sam", "Winchester", "Algebra");
+            Student studentFour = new Student("Dean", "Winchester", "Music");
+
+            // Print Student
+            Console.WriteLine(student);
+                                        
+            // Register Student
+            string register = classroom.RegisterStudent(student);
+            Console.WriteLine(register);
+
+            string registerTwo = classroom.RegisterStudent(studentTwo);
+            string registerThree = classroom.RegisterStudent(studentThree);
+            string registerFour = classroom.RegisterStudent(studentFour);
+
+            // Dismiss Student
+            string dismissed = classroom.DismissStudent("Peter", "Parker");
+            Console.WriteLine(dismissed); 
+            string dismissedTwo = classroom.DismissStudent("Ellie", "Goulding");
+            Console.WriteLine(dismissedTwo);
+            
+            // Subject info
+            string subjectInfo = classroom.GetSubjectInfo("Algebra");
+            Console.WriteLine(subjectInfo);
+
+            string anotherInfo = classroom.GetSubjectInfo("Art");
+            Console.WriteLine(anotherInfo);
+                                            
+            // Get Student
+            Console.WriteLine(classroom.GetStudent("Dean", "Winchester"));
+        }
+    }
+}
