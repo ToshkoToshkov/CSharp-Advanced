a11e067526caece50a49fca1b961f7019eee4320
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 11:05:41 +0200

    Add files via upload

---
+++ 08.FunctionalProgramming-Exercises/03.CustomMinFunction/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<List<int>, int> minNumbers = numbers =>
+            {
+                int minNumber = int.MaxValue;
+
+                foreach (int num in numbers)
+                {
+                    if (num < minNumber)
+                    {
+                        minNumber = num;
+                    }
+                }
+
+                return minNumber;
+            };
+
+            List<int> list = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int numNumber = minNumbers(list);
+            Console.WriteLine(numNumber);
+        }
+    }
+}
