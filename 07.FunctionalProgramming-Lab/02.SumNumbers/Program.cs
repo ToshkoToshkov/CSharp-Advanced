7f46a0f9d6d2a949fd72d788d1dc861d9f77f09a
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 10:41:10 +0200

    Add files via upload

---
+++ 07.FunctionalProgramming-Lab/02.SumNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSumAndCount(int.Parse,
+                a => a.Length,
+                array => array.Sum());
+        }
+
+        static void PrintSumAndCount(Func<string, int> parser, Func<int[], int> countGetter,
+          Func<int[], int> sumCalulator)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToArray();
+
+            Console.WriteLine(countGetter(input));
+            Console.WriteLine(sumCalulator(input));
+        }
+    }
+}
