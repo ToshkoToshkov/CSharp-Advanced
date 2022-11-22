f52db4e1958a0f6eac5ce987679b4bcc8acd4fb4
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 11:09:17 +0200

    Add files via upload

---
+++ 08.FunctionalProgramming-Exercises/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+            numbers.Reverse();
+
+            numbers = numbers.Where(num => num % n != 0).ToList();
+
+            Action<List<int>> print = nums => Console.WriteLine(string.Join(" ", nums));
+
+            print(numbers);
+        }
+    }
+}
