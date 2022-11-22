60f863eb9a618c7b94b519df39c3554a5cd071a6
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 09:10:48 +0200

    Add files via upload

---
+++ 01.StacksAndQueues-Lab/03.SimpleCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var value = input.Split(' ');
+
+            var stack = new Stack<string>(value.Reverse());
+
+            while (stack.Count > 1)
+            {
+                int first = int.Parse(stack.Pop());
+                string oper = stack.Pop();
+                int second = int.Parse(stack.Pop());
+
+                if (oper == "+")
+                {
+                    stack.Push((first + second).ToString());
+                }
+                if (oper == "-")
+                {
+                    stack.Push((first - second).ToString());
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
