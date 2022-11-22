f21097fb433d5806515f45502edca8c46939e813
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 10:23:07 +0200

    Add files via upload

---
+++ 05.SetsAndDictionaries-Lab/07.SoftUniParty/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> partyList = new HashSet<string>();
+
+            while (true)
+            {
+                string commands = Console.ReadLine();
+                if (commands == "PARTY")
+                {
+                    break;
+                }
+                partyList.Add(commands);
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+                if (partyList.Contains(input))
+                {
+                    partyList.Remove(input);
+                }
+            }
+
+            Console.WriteLine(partyList.Count);
+
+            foreach (var item in partyList)
+            {
+                char[] ch = item.ToCharArray();
+                if (char.IsDigit(ch[0]))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            foreach (var item in partyList)
+            {
+                char[] ch = item.ToCharArray();
+                if (char.IsLetter(ch[0]))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
