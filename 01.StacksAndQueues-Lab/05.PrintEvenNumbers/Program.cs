f7487585fb0368ad8202c70c3db473df15e89f8b
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 09:12:05 +0200

    Add files via upload

---
+++ 01.StacksAndQueues-Lab/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>(nums);
+
+            int count = queue.Sum();
+
+            int sum = 0;
+
+            while (sum != count)
+            {
+                sum += queue.Peek();
+                if (queue.Peek() % 2 == 1)
+                {
+                    queue.Dequeue();
+                }
+                else
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+            }
+            Console.WriteLine(string.Join(", ", queue));
+
+        }
+    }
+}
