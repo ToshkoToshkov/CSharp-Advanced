9bfecf028324a81c98e904f26df5e74c68126dba
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 10:13:02 +0200

    Add files via upload

---
+++ 05.SetsAndDictionaries-Lab/01.CountTheSameValueInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CountTheSameValueInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> ocurrence = new Dictionary<double, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (!ocurrence.ContainsKey(numbers[i]))
+                {
+                    ocurrence.Add(numbers[i], 0);
+                }
+
+                ocurrence[numbers[i]]++;
+            }
+
+            foreach (var item in ocurrence)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
