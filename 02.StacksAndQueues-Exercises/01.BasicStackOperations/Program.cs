5852d4145801a7a2d9e072369ccdc260675d14e2
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 09:21:49 +0200

    Add files via upload

---
+++ 02.StacksAndQueues-Exercises/01.BasicStackOperations/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = data[0];
+            int s = data[1];
+            int x = data[2];
+
+            int[] numbersInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(numbersInput[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else if (stack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else if(!stack.Contains(x))
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
