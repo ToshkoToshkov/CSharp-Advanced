501652fc41b8032e54d3e0d25220818c3bf8819e
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 11:08:27 +0200

    Add files via upload

---
+++ 08.FunctionalProgramming-Exercises/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+
+namespace AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Func<int, int> arithmetic = num => num;
+            Action<List<int>> print = nums => Console.WriteLine(string.Join(" ", nums));
+
+            string cmd = Console.ReadLine();
+
+            while (cmd != "end")
+            {
+                if (cmd == "add")
+                {
+                    arithmetic = num => num + 1;
+                    numbers = numbers.Select(
+                        n => arithmetic(n)).ToList();
+                }
+                else if (cmd == "multiply")
+                {
+                    arithmetic = num => num * 2;
+                    numbers = numbers.Select(
+                       n => arithmetic(n)).ToList();
+                }
+                else if (cmd == "subtract")
+                {
+                    arithmetic = num => num - 1;
+                    numbers = numbers.Select(
+                       n => arithmetic(n)).ToList();
+                }
+                else if (cmd == "print")
+                {
+                    print(numbers);
+                }
+
+                cmd = Console.ReadLine();
+            }
+        }
+    }
+}
