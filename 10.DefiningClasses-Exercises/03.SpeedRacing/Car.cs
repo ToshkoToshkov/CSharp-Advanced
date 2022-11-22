b6cc15cd504077f7dc87bd3ef086f8aebe094494
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 11:24:33 +0200

    Add files via upload

---
+++ 10.DefiningClasses-Exercises/03.SpeedRacing/Car.cs
@@ -0,0 +1,29 @@
+﻿namespace _03.SpeedRacing
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        public double TraveledDistance { get; set; }
+
+
+        public bool Drive(double distanceTravelled)
+        {
+            double neededFuel = distanceTravelled * this.FuelConsumptionPerKilometer;
+
+            if (neededFuel > this.FuelAmount)
+            {
+                return false;
+            }
+
+            this.TraveledDistance += distanceTravelled;
+            this.FuelAmount -= neededFuel;
+
+            return true;
+        }
+    }
+}
---
+++ 10.DefiningClasses-Exercises/03.SpeedRacing/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace _03.SpeedRacing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string model = input[0];
+                double fuelAmount = double.Parse(input[1]);
+                double fuelConsumptionPerKilometer = double.Parse(input[2]);
+
+                Car currCar = new Car
+                {
+                    Model = model,
+                    FuelAmount = fuelAmount,
+                    FuelConsumptionPerKilometer = fuelConsumptionPerKilometer
+                };
+
+                cars.Add(currCar);
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] commandData = command.Split();
+
+                string model = commandData[1];
+                double distanceTraveled = double.Parse(commandData[2]);
+
+                Car car = cars.First(c => c.Model == model);
+                bool isDrive = car.Drive(distanceTraveled);
+
+                if (isDrive == false)
+                {
+                    Console.WriteLine("Insufficient fuel for the drive");
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TraveledDistance}");
+            }
+
+        }
+    }
+}
