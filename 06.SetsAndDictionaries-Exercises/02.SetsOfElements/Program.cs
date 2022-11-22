724f0ac0c3529cc6fe216a4102dad2e0d53a0324
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 10:27:50 +0200

    Add files via upload

---
+++ 06.SetsAndDictionaries-Exercises/02.SetsOfElements/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = input[0];
+            int m = input[1];
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < n + m; i++)
+            {
+                int currInput = int.Parse(Console.ReadLine());
+
+                if (i < n)
+                {
+                    firstSet.Add(currInput);
+                }
+                else
+                {
+                    secondSet.Add(currInput);
+                }
+            }
+
+            foreach (var item in firstSet)
+            {
+                if (secondSet.Contains(item))
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+    }
+}
