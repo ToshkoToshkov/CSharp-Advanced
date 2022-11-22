6fa70a9db95ba30257d8dd61b5babb8f591af6ad
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 10:13:58 +0200

    Add files via upload

---
+++ 05.SetsAndDictionaries-Lab/02.AverageStudentsGrades/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<Decimal>> dic = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                var name = input[0];
+                Decimal grade = Decimal.Parse(input[1]);
+
+                if (!dic.ContainsKey(name))
+                {
+                    dic.Add(name, new List<decimal>());
+                }
+
+                dic[name].Add(grade);
+
+            }
+
+            foreach (var name in dic)
+            {
+                Console.Write($"{name.Key} -> ");
+
+                foreach (var item in name.Value)
+                {
+                    Console.Write($"{item:f2} ");
+                }
+
+                Console.WriteLine($"(avg: {name.Value.Average():f2})");
+            }
+        }
+    }
+}
