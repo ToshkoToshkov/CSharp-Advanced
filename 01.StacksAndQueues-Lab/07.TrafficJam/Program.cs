4d4de499665bd703869cfaa043822245edbc371c
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 09:16:17 +0200

    Add files via upload

---
+++ 01.StacksAndQueues-Lab/07.TrafficJam/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> cars = new Queue<string>();
+
+            int count = 0;
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        count++;
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
