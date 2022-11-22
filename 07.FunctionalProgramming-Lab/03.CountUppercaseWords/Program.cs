ed407c2715016b5125283ee643b6fda431334585
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 10:43:48 +0200

    Add files via upload

---
+++ 07.FunctionalProgramming-Lab/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> filter = text => Char.IsUpper(text[0]);
+
+            string input = Console.ReadLine();
+            string[] words = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            words = words.Where(filter).ToArray();
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+        }
+    }
+}
