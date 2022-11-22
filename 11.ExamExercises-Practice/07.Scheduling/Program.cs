75b09c661bc530f1aabd475cc1b82c84ae1a49e7
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 11:38:24 +0200

    Add files via upload

---
+++ 11.ExamExercises-Practice/07.Scheduling/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _07.Scheduling
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] input1 = Console.ReadLine()
+                .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] input2 = Console.ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int valueOfTheTask = int.Parse(Console.ReadLine());
+
+            Stack<int> tasks = new Stack<int>(input1);
+            Queue<int> thread = new Queue<int>(input2);
+
+            while (thread.Count != 0 && tasks.Count != 0)
+            {
+                if (tasks.Peek() == valueOfTheTask)
+                {
+                    Console.WriteLine("Thread with value {0} killed task {1}", thread.Peek(), valueOfTheTask);
+
+                    foreach (var item in thread)
+                    {
+                        Console.Write(item + " ");
+                    }
+
+                    break;
+                }
+
+                if (thread.Peek() >= tasks.Peek())
+                {
+                    thread.Dequeue();
+                    tasks.Pop();
+                }
+                else
+                {
+                    thread.Dequeue();
+                }
+            }
+        }
+    }
+}
+
+
+
