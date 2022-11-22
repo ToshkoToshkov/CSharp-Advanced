b033a6736d7f39f1f5b63df5193687b60d30bffa
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 10:22:29 +0200

    Add files via upload

---
+++ 05.SetsAndDictionaries-Lab/06.ParkingLot/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            HashSet<string> carsNumber = new HashSet<string>();
+
+            while (command != "END")
+            {
+                string[] currInput = command.Split(", ");
+
+                string currCommand = currInput[0];
+                string number = currInput[1];
+
+                if (currCommand == "IN")
+                {
+                    carsNumber.Add(number);
+                }
+
+                if (currCommand == "OUT")
+                {
+                    if (carsNumber.Contains(number))
+                    {
+                        carsNumber.Remove(number);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+
+            if (carsNumber.Count > 0)
+            {
+                foreach (var item in carsNumber)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
