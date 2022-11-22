1d17b476a0477ae719846a5dd04ee40f1a5b4d10
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 09:52:19 +0200

    Add files via upload

---
+++ 04.MultidimensionalArray-Exercises/01.DiagonalDifference/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            int primaryDiagonal = 0;
+            int secondaryDiagonal = 0;
+            int counter = n - 1;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                primaryDiagonal += matrix[row, row];
+
+                secondaryDiagonal += matrix[row, counter];
+                counter--;
+            }
+
+            Console.WriteLine(Math.Abs(primaryDiagonal - secondaryDiagonal));
+            ;
+        }
+    }
+}
