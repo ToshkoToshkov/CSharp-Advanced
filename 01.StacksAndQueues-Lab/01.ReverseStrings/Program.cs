9e888c90d8a9bd3275529d8d852a67ab4951d80b
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 09:09:19 +0200

    Add files via upload

---
+++ 01.StacksAndQueues-Lab/01.ReverseStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> reversed = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                reversed.Push(input[i]);
+            }
+
+            while (reversed.Count != 0)
+            {
+                Console.Write(reversed.Pop());
+            }
+        }
+    }
+}
