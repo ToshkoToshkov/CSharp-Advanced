6924ce88f4afc7080744fbd0da07c288c63dc162
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 11:23:59 +0200

    Add files via upload

---
+++ 10.DefiningClasses-Exercises/02.DateModifier/DateModifier.cs
@@ -0,0 +1,14 @@
+﻿namespace _02.DateModifier
+{
+    public static class DateModifier
+    {
+        public static int GetDiffBetweenDatesInDays(string dateOneStr, string dateTwoStr)
+        {
+            DateTime dateOne = DateTime.Parse(dateOneStr);
+            DateTime dateTwo = DateTime.Parse(dateTwoStr);
+
+            TimeSpan diff = dateOne - dateTwo;
+            return Math.Abs(diff.Days);
+        }
+    }
+}
---
+++ 10.DefiningClasses-Exercises/02.DateModifier/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _02.DateModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            int days = DateModifier.GetDiffBetweenDatesInDays(firstDate, secondDate);
+
+            Console.WriteLine(days);
+        }
+    }
+}
