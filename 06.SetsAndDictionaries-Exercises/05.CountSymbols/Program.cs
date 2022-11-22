4c0b22d7adcfc7ce4ae36ef832ae3fe0c0e77fd6
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 10:32:07 +0200

    Add files via upload

---
+++ 06.SetsAndDictionaries-Exercises/05.CountSymbols/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            char[] arr = input.ToArray();
+
+            SortedDictionary<char, int> dic = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!dic.ContainsKey(arr[i]))
+                {
+                    dic.Add(arr[i], 0);
+                }
+
+                dic[arr[i]]++;
+            }
+
+            foreach (var item in dic)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
