e9b5524163245c5d95f19b139cb561d0cbf5b295
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 10:44:59 +0200

    Add files via upload

---
+++ 07.FunctionalProgramming-Lab/04.AddVat/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AddVat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .Select(n => n + (n * 0.2m))
+                .ToArray();
+
+            foreach (var item in input)
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+        }
+    }
+}
