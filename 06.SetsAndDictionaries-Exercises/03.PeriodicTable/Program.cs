4002202defb4e793d3c4f0dde4ade53ac2233d5b
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 10:28:26 +0200

    Add files via upload

---
+++ 06.SetsAndDictionaries-Exercises/03.PeriodicTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> elements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    elements.Add(input[j]);
+                }
+            }
+
+            foreach (var item in elements)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
