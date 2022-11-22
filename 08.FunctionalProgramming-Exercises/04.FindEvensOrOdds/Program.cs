f9d9e48813f9fde0adcccdd8d982a6c7ba9dfdc9
Toshko Toshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2022-11-22 11:07:10 +0200

    Add files via upload

---
+++ 08.FunctionalProgramming-Exercises/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace FindEvenOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ranges = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int start = ranges[0];
+            int end = ranges[1];
+
+            Func<int, int, List<int>> generateRangOfNums = (s, e) =>
+            {
+                List<int> nums = new List<int>();
+
+                for (int i = s; i <= e; i++)
+                {
+                    nums.Add(i);
+                }
+
+                return nums;
+            };
+
+            List<int> numbers = generateRangOfNums(start, end);
+
+            string command = Console.ReadLine();
+
+            if (command == "even")
+            {
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    if (numbers[i] % 2 == 0)
+                    {
+                        Console.Write(numbers[i] + " ");
+                    }
+                }
+            }
+            else if (command == "odd")
+            {
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    if (numbers[i] % 2 != 0)
+                    {
+                        Console.Write(numbers[i] + " ");
+                    }
+                }
+            }
+
+        }
+    }
+}
